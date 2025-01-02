c5272164cf1fcc4b90ea5401dd69dec2057b077b
Pho Hale <PhoHale@gmail.com>
2025-01-01 20:04:12 -0500

    workspace

--- src/platform/common/constants.ts
+++ src/platform/common/constants.ts
@@ -25,7 +25,8 @@ export const JVSC_EXTENSION_ID = 'ms-toolsai.jupyter';
 export const DATA_WRANGLER_EXTENSION_ID = 'ms-toolsai.datawrangler';
 export const POWER_TOYS_EXTENSION_ID = 'ms-toolsai.vscode-jupyter-powertoys';
 export const JUPYTER_HUB_EXTENSION_ID = 'ms-toolsai.jupyter-hub';
-export const POWER_TOYS_EXTENSION_ID = 'phohale.vscode-jupyter-powertoys';
+// export const JVSC_EXTENSION_ID = 'phohale.jupyter';
+// export const POWER_TOYS_EXTENSION_ID = 'phohale.vscode-jupyter-powertoys';
 export const AppinsightsKey = '0c6ae279ed8443289764825290e4f9e2-1a736e7c-1324-4338-be46-fc2a58ae4d14-7255';
 
 export const STANDARD_OUTPUT_CHANNEL = 'STANDARD_OUTPUT_CHANNEL';
